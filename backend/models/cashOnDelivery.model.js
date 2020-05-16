4992b649e8501037fce71298480585d001a12b0b
JanithRushantha <janithrushantha@gmail.com>
2020-05-16 23:40:06 +0530

    Janith Backend

---
+++ backend/models/cashOnDelivery.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const cashOnDeliverySchema = new Schema({
+    deliveryAddress: { type: String, required: true, minlength: 5 }
+}, {
+    timestamps: true,
+});
+
+const CashOnDelivery = mongoose.model('CashOnDelivery', cashOnDeliverySchema);
+
+module.exports = CashOnDelivery;
