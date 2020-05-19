1af30f0c13bbaaef446bff6c55b4d5a3777c62e9
arojayathilake <acjayathilake@gmail.com>
2020-05-20 03:34:06 +0530

    add-product.js edit-product.js styling added

--- src/components/store-manager/products-list.js
+++ src/components/store-manager/products-list.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import '../../stylesheets/products-products-list.css'
 
 class ProductsList extends Component {
 
@@ -61,10 +62,10 @@ class ProductsList extends Component {
     render() {
         const products = this.state.allProducts.filter(product => (product.category === this.props.match.params.category));
         return(
-            <div>
-                <h2>Products List</h2>
-                <table className="table table-dark table-hover">
-                    <thead>
+            <div className="container mt-5" >
+                <h3 style={{color: "#4A235A"}}>Products List</h3>
+                <table className="table table-hover" style={{backgroundColor:"#EBDEF0"}}>
+                    <thead style={{backgroundColor:"#AF7AC5"}}>
                     <tr>
                         <th scope="col">Name</th>
                         <th scope="col">Price</th>
@@ -84,24 +85,43 @@ class ProductsList extends Component {
                                     className="btn btn-secondary"
                                 >
                                     Edit
-                                </a> | <button
-                                    className="btn btn-danger"
-                                    onClick={() => this.deleteProductImage(this.state.imageIDs[product.prodId-1])}
-                                >
-                                    Delete Product Image
-                                </button> | <button
-                                    className="btn btn-danger"
-                                    onClick={() => this.deleteProduct(product._id)}
-                                >
-                                    Delete Product
-                                </button>
+                                </a>
+                                {/*| <button*/}
+                                {/*    className="btn btn-danger"*/}
+                                {/*    onClick={() => this.deleteProductImage(this.state.imageIDs[product.prodId-1])}*/}
+                                {/*>*/}
+                                {/*    Delete Product Image*/}
+                                {/*</button> | <button*/}
+                                {/*    className="btn btn-danger"*/}
+                                {/*    onClick={() => this.deleteProduct(product._id)}*/}
+                                {/*>*/}
+                                {/*    Delete Product*/}
+                                {/*</button>*/}
                             </td>
                         </tr>
                     ))}
                     </tbody>
                 </table>
             </div>
-
+            // products.map(product => (
+            //     <div key={product.prodId}>
+            //         <div>
+            //             <div className="product">
+            //                 <div className="productImage">
+            //                     <img
+            //                         src={this.state.image}
+            //                         // src={img}
+            //                         alt={product.name}/>
+            //                 </div>
+            //                 <div className="productDetails">
+            //                     <p>{product.name}</p>
+            //                     <p>{product.price}</p>
+            //                     <p>{product.discount}</p>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // ))
         );
     }
 }
