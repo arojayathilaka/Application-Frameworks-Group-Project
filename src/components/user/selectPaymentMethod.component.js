a4fffb940adb25ea605c42f2ddd5ed588c2fbd4c
JanithRushantha <janithrushantha@gmail.com>
2020-05-23 23:43:38 +0530

    Comments fix, UI improvements, Payment method improved

--- src/components/user/selectPaymentMethod.component.js
+++ src/components/user/selectPaymentMethod.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import cashOnDelivery from "./cashOnDelivery.component";
 
 function Application(){
 
@@ -12,16 +11,18 @@ function Application(){
 
     return(
         <div>
-            <br/><br/><br/><br/><br/><br/><br/><br/>
-            <div id="pmethodselect">
-                <h1 align="center">Select Payment Method</h1>
-                <br/><br/>
-                <div className="form-group" align="center">
-                    <button type="submit" className="btn btn-primary btn-lg" onClick={cashDelivery} style={{backgroundColor: "#AF7AC5"}}>Credit Card Payments</button>
-                </div>
-                <br/><br/><br/>
-                <div className="form-group" align="center">
-                    <button type="submit" className="btn btn-primary btn-lg " onClick={cardPayment} style={{backgroundColor: "#AF7AC5"}}>Cash On Delivery    </button>
+            <br/><br/><br/><br/><br/><br/><br/>
+            <div className="container">
+                <div className="jumbotron" style={{backgroundColor:"#E8F8F5"}}>
+                    <h1 align="center">Select Payment Method</h1>
+                    <br/><br/>
+                    <div className="form-group" align="center">
+                        <button type="submit" className="btn btn-primary btn-lg" onClick={cashDelivery} style={{backgroundColor: "#AF7AC5"}}>Credit Card Payments</button>
+                    </div>
+                    <br/><br/><br/>
+                    <div className="form-group" align="center">
+                        <button type="submit" className="btn btn-primary btn-lg " onClick={cardPayment} style={{backgroundColor: "#AF7AC5"}}>Cash On Delivery    </button>
+                    </div>
                 </div>
             </div>
         </div>
